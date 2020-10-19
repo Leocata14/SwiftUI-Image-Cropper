88c8f8fe751e139734231232c51cf75877b6f53b
Jason Leocata <jl@jasonleocata.com.au>
2020-10-20 09:46:19 +1100

    Initial Commit

---
+++ SwiftUI Image Cropper/SwiftUI_Image_CropperApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Image_CropperApp.swift
+//  SwiftUI Image Cropper
+//
+//  Created by Jason Leocata on 20/10/20.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Image_CropperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
