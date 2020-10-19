00a5e4f53b2ac2f4a82d98996165a3724e724970
Jason Leocata <jl@jasonleocata.com.au>
2020-10-20 10:01:21 +1100

    Initial commit

--- SwiftUI Image Cropper/ContentView.swift
+++ SwiftUI Image Cropper/ContentView.swift
@@ -9,13 +9,55 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        CropperView(image: UIImage(named: "demo"))
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct CropperView: View {
+    
+    @State private var scale: CGFloat = 1.0
+    @State private var degrees: Double = 0
+    @State private var offset = CGSize.zero
+    
+    let image: UIImage?
+    
+    var body: some View {
+        let magnificationGesture = MagnificationGesture()
+            .onChanged { (value) in
+            self.scale = value.magnitude
+        }
+        
+        let rotationGesture = RotationGesture(minimumAngleDelta: Angle(degrees: 5)).onChanged { (value) in
+            self.degrees = value.degrees
+        }
+        
+        let scaleAndRotateGesture = SimultaneousGesture(magnificationGesture, rotationGesture)
+        
+        let dragGesture = DragGesture().onChanged { (value) in
+            let translation = value.translation
+            print(translation)
+            self.offset = translation
+        }
+        
+        return image != nil ?
+            ZStack {
+                Image(uiImage: image!)
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .scaleEffect(scale)
+                    .rotationEffect(Angle(degrees: degrees))
+                    .offset(self.offset)
+                    .gesture(scaleAndRotateGesture)
+                    .highPriorityGesture(dragGesture)
+            }
+            .frame(width: 300, height: 300)
+            .clipped()
+         : nil
     }
 }
