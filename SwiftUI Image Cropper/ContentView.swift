37d2aa4e52e4db5251cffc090bfa00cfbf74bf4c
Jason Leocata <jl@jasonleocata.com.au>
2020-10-20 10:49:05 +1100

    Moved State to main View and save to Camera roll

--- SwiftUI Image Cropper/ContentView.swift
+++ SwiftUI Image Cropper/ContentView.swift
@@ -8,8 +8,32 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var croppedImage: UIImage? = UIImage(named: "demo")
+    
+    @State private var scale: CGFloat = 1.0
+    @State private var degrees: Double = 0
+    @State private var offset = CGSize.zero
+    
+    
     var body: some View {
-        CropperView(image: UIImage(named: "demo"))
+        VStack {
+            CropperView(scale: $scale, degrees: $degrees, offset: $offset, image: $croppedImage)
+            
+            Button {
+                save()
+            } label: {
+                Text("Convert to Image")
+            }
+
+        }
+        
+    }
+    
+    func save() {
+        let image = CropperView(scale: $scale, degrees: $degrees, offset: $offset, image: $croppedImage).asUIImage()
+        UIImageWriteToSavedPhotosAlbum(image, nil, nil, nil)
+        
     }
 }
 
@@ -21,11 +45,11 @@ struct ContentView_Previews: PreviewProvider {
 
 struct CropperView: View {
     
-    @State private var scale: CGFloat = 1.0
-    @State private var degrees: Double = 0
-    @State private var offset = CGSize.zero
+    @Binding var scale: CGFloat
+    @Binding var degrees: Double
+    @Binding var offset : CGSize
     
-    let image: UIImage?
+    @Binding var image: UIImage?
     
     var body: some View {
         let magnificationGesture = MagnificationGesture()
@@ -59,5 +83,35 @@ struct CropperView: View {
             .frame(width: 300, height: 300)
             .clipped()
          : nil
+    }
+}
+
+extension View {
+// This function changes our View to UIView, then calls another function
+// to convert the newly-made UIView to a UIImage.
+    public func asUIImage() -> UIImage {
+        let controller = UIHostingController(rootView: self)
+        
+        controller.view.frame = CGRect(x: 0, y: CGFloat(Int.max), width: 1, height: 1)
+        UIApplication.shared.windows.first!.rootViewController?.view.addSubview(controller.view)
+        
+        let size = controller.sizeThatFits(in: UIScreen.main.bounds.size)
+        controller.view.bounds = CGRect(origin: .zero, size: size)
+        controller.view.sizeToFit()
+        
+        // here is the call to the function that converts UIView to UIImage: `.asImage()`
+        let image = controller.view.asUIImage()
+        controller.view.removeFromSuperview()
+        return image
+    }
+}
+
+extension UIView {
+// This is the function to convert UIView to UIImage
+    public func asUIImage() -> UIImage {
+        let renderer = UIGraphicsImageRenderer(bounds: bounds)
+        return renderer.image { rendererContext in
+            layer.render(in: rendererContext.cgContext)
+        }
     }
 }
